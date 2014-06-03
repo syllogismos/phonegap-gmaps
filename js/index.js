9710cd313534fc241ab7956a05091d4bd52049cc
Anil Karaka <anilkaraka@live.in>
2014-06-03 22:47:09 +0530

    little bugs

--- js/index.js
+++ js/index.js
@@ -45,7 +45,7 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
+    },
     // Initialize google maps
     initializeMap: function() {
 	var mapOptions = {
